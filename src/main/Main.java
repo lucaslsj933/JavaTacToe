ebb87e1c12c8ace37ae06f52b823b749243519bd
Lucas Santos <81392739+lucaslsj933@users.noreply.github.com>
2023-06-24 15:29:29 -0300

    Início da interface

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,9 @@
 package main;
 
+import javax.swing.*;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Test");
+		MainUI mainUI=new MainUI();
 	}
 }
---
+++ src/main/MainUI.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.swing.*;
+
+public class MainUI extends JFrame{
+	public MainUI() {
+		super();
+		setSize(1280,720);
+		setLayout(null);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
